f8a4a849345b93c33418b4d17db2c46ee9ab4657
wsdsd <wellingttonnlopes@gmail.com>
2020-10-26 16:20:25 -0300

    Added live search bar for lesson create form, created lesson_Student relation

---
+++ resources/js/components/LiveSearchBar.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "LiveSearchBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TestComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
